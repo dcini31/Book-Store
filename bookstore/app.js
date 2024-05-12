1f20febe225bb3cf441dee3c7a7b0877f2a942c6
dcini31 <73096398+dcini31@users.noreply.github.com>
2024-05-12 15:14:08 +0200

    created error handling

--- bookstore/app.js
+++ bookstore/app.js
@@ -6,10 +6,27 @@ const morgan = require("morgan");
 const orderRoutes = require("./api/routes/orders");
 const customerRoutes = require("./api/routes/customers");
 
+//provides more information on console
 app.use(morgan("dev"));
 
 //Routes to handle requests
 app.use("/orders", orderRoutes);
 app.use("/customers", customerRoutes);
 
+//setting error handling middleware
+app.use((req, res, next) => {
+  const error = new Error("NOT FOUND");
+  error.status = 404;
+  next(error);
+});
+
+//handles all errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 module.exports = app;
